6f6e516f3731539fdf264d37ce166a2f31e6ba4c
Milan Giri <milan.giri17021999@gmail.com>
2021-08-25 09:43:46 +0545

    Frontend View structure managed

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\Frontend\ApplicationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace'=>'Frontend'],function(){
+    Route::any('/',[ApplicationController::class,'index'])->name('home');
 });
+
+// Route::group(['namespace'=>'Backend','prefix'='admin-section'],function(){
+
+// });
+
